38991d7195f7d64eb9e1ae630bc2efbecada40d2
Jad Chakra <jad.chakra@hdr.qut.edu.au>
2023-01-04 15:48:47 +1000

    test

--- Package.swift
+++ Package.swift
@@ -3,12 +3,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "CycloneDDS",
+    name: "cyclonedds",
     products: [
-        .library(name: "CycloneDDS", targets: ["CycloneDDS"])
+        .library(name: "cyclonedds", targets: ["cyclonedds"])
     ],
     targets: [
-        .binaryTarget(name: "CycloneDDS",
+        .binaryTarget(name: "cyclonedds",
                       url: "https://github.com/jc211/CycloneDDSPrebuild/releases/download/0.10.2/cyclonedds.xcframework.zip",
                       checksum: "35193ae9ba6bcceb3f2dcbcf2afe292d02bbb6bcc5e547167542a15c0a55be36")
     ]
